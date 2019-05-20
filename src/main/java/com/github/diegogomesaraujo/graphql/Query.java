933b609aa1abe51d56593d3aed5187e9fb08a53c
Diego Gomes Araujo <diego.gomes@tokiomarine.com.br>
2019-05-20 13:58:16 -0300

    new query to find a specific author

--- src/main/java/com/github/diegogomesaraujo/graphql/Query.java
+++ src/main/java/com/github/diegogomesaraujo/graphql/Query.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.github.diegogomesaraujo.entities.Author;
 import com.github.diegogomesaraujo.entities.Post;
+import com.github.diegogomesaraujo.repositories.AuthorDao;
 import com.github.diegogomesaraujo.repositories.PostDao;
 
 @Component
@@ -14,8 +16,16 @@ public class Query implements GraphQLQueryResolver {
 
 	@Autowired
 	private PostDao postDao;
+
+	@Autowired
+	private AuthorDao authorDao;
 	
 	public List<Post> getListPosts() {
 		return this.postDao.findAll();
 	}
+	
+	public Author getAuthorById(Long id) {
+		return authorDao.findById(id).orElse(null);
+	}
+	
 }
